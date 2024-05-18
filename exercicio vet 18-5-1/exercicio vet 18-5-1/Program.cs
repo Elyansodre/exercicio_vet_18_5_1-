2af3e327d0d68e33fc47c4f052307f7af2ff918e
Elyansodre <128232034+Elyansodre@users.noreply.github.com>
2024-05-18 16:51:54 -0300

    Add files via upload

---
+++ exercicio vet 18-5-1/exercicio vet 18-5-1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_vet_18_5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[N];
+            int[] idades = new int[N];
+            double[] alturas = new double[N];
+
+            //Leitura dos dados
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                nomes[i] = s[0];
+                idades[i] = int.Parse(s[1]);
+                alturas[i] = double.Parse(s[2], CultureInfo.InvariantCulture);
+            }
+
+            //Calculo da altura medi das pessoas
+
+            double soma = 0.0;
+            for (int i = 0; i < N; i++)
+            {
+                soma = soma + alturas[1];
+            }
+
+            //Porcentagem de pessoas abaixo de 16 anos
+            int cont = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (idades[i] < 16)
+                {
+                    cont++;
+                }
+            }
+            double porcentagem = (double)cont / N * 100.0;
+            Console.WriteLine("Pessoas com menos de 16 anos: " + porcentagem.ToString("F1", CultureInfo.InvariantCulture) + "%");
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
+
